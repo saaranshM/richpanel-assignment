4a0510b716e5af26ad8da43f8ceac4e830444b4f
Saaransh <saaransh.menon2000@gmail.com>
2021-08-29 02:55:01 +0530

    added home page

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import Axios from "axios";
 import router from "./routes/router";
 import store from "./store/store";
+import LottieVuePlayer from "@lottiefiles/vue-lottie-player";
 
+Vue.use(LottieVuePlayer);
 Vue.prototype.$axios = Axios;
 
 Vue.config.productionTip = false;
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
-    Home
-    <h2>{{ user.first_name }}</h2>
-  </div>
+  <section class="home">
+    <div>
+      <h1 class="home_welcome">Welcome!</h1>
+      <h2 class="home_hey">
+        Hey there {{ user.first_name }} we are getting things ready for you....
+      </h2>
+      <lottie-vue-player
+        :src="`https://assets2.lottiefiles.com/packages/lf20_tilm0top.json`"
+        :theme="options.theme"
+        :player-size="options.playerSize"
+        class="home_loading"
+      />
+      <lottie-vue-player
+        :src="`https://assets5.lottiefiles.com/packages/lf20_bBEnuW.json`"
+        :theme="options.theme"
+        :player-size="options.playerSize"
+        class="home_loading"
+      />
+    </div>
+  </section>
 </template>
 
 <script>
@@ -10,6 +26,26 @@ export default {
   data() {
     return {
       userData: null,
+      options: {
+        minimizable: false,
+        playerSize: "standard",
+        backgroundColor: "#fff",
+        backgroundStyle: "color",
+        theme: {
+          controlsView: "hide",
+          active: "light",
+          light: {
+            color: "#e6f1ff",
+            backgroundColor: "#fff",
+            opacity: "0.7",
+          },
+          dark: {
+            color: "#fff",
+            backgroundColor: "#e6f1ff",
+            opacity: "0.7",
+          },
+        },
+      },
     };
   },
   async mounted() {
